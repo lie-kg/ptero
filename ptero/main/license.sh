19aae104815530b264a1f3266b5c6a12b04e6876
lie_kg <fetgeryrd0187@gmail.com>
2026-05-02 21:17:16 +0300

    Update license.sh

--- ptero/main/license.sh
+++ ptero/main/license.sh
@@ -1,2 +1,2 @@
 
-bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/main/dev.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/lie-kg/ptero/refs/heads/main/ptero/main/dev.sh)
